740abd994c2ac71710aa469292fdf0ed91300e9f
smurphy123 <smurphysmail@gmail.com>
2021-04-24 14:09:41 -0400

    Repo Update

--- PythonWorkout/guessing_game.py
+++ PythonWorkout/guessing_game.py
@@ -4,15 +4,22 @@ def guessing_game():
     magic_num = random.randint(0,101)
     correct_guess = False
 
-    user_guess = input("Guess a number between 0 and 100: ")
+    guess = input("Guess a number between 0 and 100: ")
+    user_guess = int(guess)
 
     while(correct_guess == False):
 
         if(user_guess > magic_num):
             print("Your guess is too high. Ty again.")
+            guess = input("New guess: ")
+            user_guess = int(guess)
         elif (user_guess < magic_num): 
             print("Your guess is too low. Try again.")
+            guess = input("New guess: ")
+            user_guess = int(guess)
         else:
             correct_guess = True
 
     print("Congratulations! You guessed the magic number!")
+
+guessing_game()
