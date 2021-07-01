5b62463ee6db6c334787b8b8a74e08f93f0ed675
smurphy123 <smurphysmail@gmail.com>
2021-06-30 22:30:13 -0400

    Repo Update

--- PythonWorkout/NumericTypes/starts_with_vowel.py
+++ PythonWorkout/NumericTypes/starts_with_vowel.py
@@ -3,3 +3,7 @@ for vowel in 'aeiou':
     if word[0] == vowel:
         starts_with_vowel = True
         break
+
+#if (word[0] == 'a' or word[0] == 'e' or
+#       word[0] == 'i' or word[0] == 'o' or word[0] == 'u'):
+#    break
