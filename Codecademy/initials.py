699b1d318d3f7508c5e7319a084d603ca8b6b3c4
smurphy123 <smurphysmail@gmail.com>
2021-09-10 22:26:11 -0400

    Repo Update

---
+++ Codecademy/initials.py
@@ -0,0 +1,20 @@
+# Mike Smith
+# I'm a professional sky diver
+
+mBlock = [[1,0,0,0,1],[1,1,0,1,1],[1,1,0,1,1],[1,0,1,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1]]
+sBlock = [[0,1,1,1,0],[1,0,0,0,1],[1,0,0,0,0],[0,1,1,1,0],[0,0,0,0,1],[1,0,0,0,1],[0,1,1,1,0]]
+
+for column in range(7):
+  print('')
+  for row in range(5):
+    if(mBlock[column][row] == 1):
+      print('M', end='')
+    else:
+      print(' ', end='')
+
+  for row in range(5):
+    if(sBlock[column][row] == 1):
+      print('S', end='')
+    else:
+      print(' ', end='')
+  
